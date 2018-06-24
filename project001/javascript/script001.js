3b21331eddeaf7881185a27d9acd57346ef751a4
Ranick Patra <ranickpatra.nalikul@gmail.com>
2018-06-24 13:58:34 +0530

    first commit

---
+++ project001/javascript/script001.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+
+/*  $(".code_area").mouseenter(function() {
+    $(this).addClass("w3-card-4");
+    $(".nav-tabs").addClass("w3-card-4");
+  }).mouseleave(function() {
+    $(this).removeClass("w3-card-4");
+    $(".nav-tabs").removeClass("w3-card-4");
+  });
+
+  $(".nav-tabs").mouseenter(function() {
+    $(this).addClass("w3-card-4");
+    $(".code_area").addClass("w3-card-4");
+  }).mouseleave(function() {
+    $(this).removeClass("w3-card-4");
+    $(".code_area").removeClass("w3-card-4");
+  });
+
+  $(".output").mouseenter(function(){
+    $(this).addClass("w3-card-4");
+  }).mouseleave(function() {
+    $(this).removeClass("w3-card-4");
+  });*/
+
+
+
+  /*$(".articles").mouseenter(function(){
+    $(this).addClass("w3-card-4");
+  }).mouseleave(function() {
+    $(this).removeClass("w3-card-4");
+  });*/
+
+});
